e038f82bd4cd9426f36b957ae3532c28c04e3e3b
Daniel Torres <DanielAlexanderTorres817@gmail.com>
2024-11-25 19:03:04 -0500

    added landing page

--- views.py
+++ views.py
@@ -5,3 +5,11 @@ views =Blueprint(__name__, "views")
 @views.route("/")
 def login():
     return render_template("login.html")
+
+@views.route("/landing")
+def home():
+    return render_template("landing.html")
+
+# @views.route("/home")
+# def home():
+#     return render_template("home.html")
