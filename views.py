79adfaee4cd1dda0abd589a03724c2770d26ade6
Daniel Torres <DanielAlexanderTorres817@gmail.com>
2024-11-25 19:41:36 -0500

    fixed app structure

--- views.py
+++ views.py
@@ -3,12 +3,12 @@ from flask import Blueprint, render_template
 views =Blueprint(__name__, "views")
 
 @views.route("/")
+def landing():
+    return render_template("landing.html")
+
+@views.route("/login")
 def login():
     return render_template("login.html")
-
-@views.route("/landing")
-def home():
-    return render_template("landing.html")
 
 # @views.route("/home")
 # def home():
